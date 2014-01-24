0ec3fa32e599b7b06d9aed5857aba98878d59124
Kamol Mavlonov <kamol@701search.com>
2014-01-24 22:12:12 +0800

    Fired Facebook LoginButton with NestedFragment

--- Nefete/src/main/java/org/kamol/nefete/ui/fragment/ListAdFragment.java
+++ Nefete/src/main/java/org/kamol/nefete/ui/fragment/ListAdFragment.java
@@ -1,13 +1,45 @@
 package org.kamol.nefete.ui.fragment;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.widget.LoginButton;
 import org.kamol.nefete.R;
 
 public class ListAdFragment extends Fragment {
+    private static final String TAG = "ListAdFragment";
+
+    private UiLifecycleHelper uiHelper;
+
+    private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+        if (state.isOpened()) {
+            Log.i(TAG, "Logged in...");
+        } else if (state.isClosed()) {
+            Log.i(TAG, "Logged out...");
+        }
+    }
+
+    private Session.StatusCallback callback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state, Exception exception) {
+            onSessionStateChange(session, state, exception);
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        uiHelper = new UiLifecycleHelper(getActivity(), callback);
+        uiHelper.onCreate(savedInstanceState);
+    }
 
     public static ListAdFragment newInstance() {
         return new ListAdFragment();
@@ -15,6 +47,54 @@ public class ListAdFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_list_ad, container, false);
+        View view = inflater.inflate(R.layout.fragment_splash, container, false);
+        LoginButton lbFacebook = (LoginButton) view.findViewById(R.id.lb_facebook);
+        lbFacebook.setFragment(this);
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // For scenarios where the main activity is launched and user
+        // session is not null, the session state change notification
+        // may not be triggered. Trigger it if it's open/closed.
+        Session session = Session.getActiveSession();
+        if (session != null &&
+                (session.isOpened() || session.isClosed()) ) {
+            onSessionStateChange(session, session.getState(), null);
+        }
+
+        uiHelper.onResume();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiHelper.onSaveInstanceState(outState);
+    }
+
+    private void showSplashFragment() {
+        Fragment splashFragment = new SplashFragment();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.add(R.id.rl_list_ad, splashFragment).commit();
     }
 }
--- Nefete/src/main/java/org/kamol/nefete/ui/fragment/SplashFragment.java
+++ Nefete/src/main/java/org/kamol/nefete/ui/fragment/SplashFragment.java
@@ -1,20 +1,29 @@
 package org.kamol.nefete.ui.fragment;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import org.kamol.nefete.R;
 
 public class SplashFragment extends Fragment {
 
-    public static SplashFragment newInstance() {
-        return new SplashFragment();
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_splash, container, false);
+        View root = inflater.inflate(R.layout.fragment_splash, container, false);
+        ImageView ivTest = (ImageView) root.findViewById(R.id.iv_test);
+        ivTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                destroyFragment();
+            }
+        });
+        return root;
+    }
+
+    private void destroyFragment() {
+        getChildFragmentManager().beginTransaction().hide(this).commit();
     }
 }
