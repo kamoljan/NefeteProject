77a85935d4b448cc64326a1362f30842a5a27226
Kamol Mavlonov <kamol@701search.com>
2014-03-17 17:09:36 +0800

    LISTING: Added Pinterest style List View

---
+++ Nefete/src/main/java/org/kamol/nefete/adapter/SampleAdapter.java
@@ -0,0 +1,105 @@
+package org.kamol.nefete.adapter;
+
+import android.content.Context;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.etsy.android.grid.util.DynamicHeightImageView;
+import com.etsy.android.grid.util.DynamicHeightTextView;
+import com.squareup.picasso.Picasso;
+
+import org.kamol.nefete.R;
+import org.kamol.nefete.http.GoRestClient;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/***
+ * PINTEREST ADAPTER
+ */
+public class SampleAdapter extends ArrayAdapter<String> {
+
+    private static final String TAG = "SampleAdapter";
+
+    static class ViewHolder {
+        DynamicHeightTextView txtLineOne;
+        DynamicHeightImageView mImageView;
+    }
+
+    private final LayoutInflater mLayoutInflater;
+    private final Random mRandom;
+    private final ArrayList<Integer> mBackgroundColors;
+
+    private static final SparseArray<Double> sPositionHeightRatios = new SparseArray<Double>();
+
+    public SampleAdapter(final Context context, final int textViewResourceId) {
+        super(context, textViewResourceId);
+        mLayoutInflater = LayoutInflater.from(context);
+        mRandom = new Random();
+        mBackgroundColors = new ArrayList<Integer>();
+        mBackgroundColors.add(R.color.orange);
+        mBackgroundColors.add(R.color.green);
+        mBackgroundColors.add(R.color.blue);
+        mBackgroundColors.add(R.color.yellow);
+        mBackgroundColors.add(R.color.grey);
+    }
+
+    @Override
+    public View getView(final int position, View convertView, final ViewGroup parent) {
+        ViewHolder vh;
+        if (convertView == null) {
+            convertView = mLayoutInflater.inflate(R.layout.list_item_sample, parent, false);
+            vh = new ViewHolder();
+            vh.txtLineOne = (DynamicHeightTextView) convertView.findViewById(R.id.txt_line1);
+            vh.mImageView = (DynamicHeightImageView) convertView.findViewById(R.id.iv_image);
+            // Trigger the download of the URL asynchronously into the image view.
+            Picasso.with(this.getContext())
+                    .load(GoRestClient.getAbsoluteUrl(":9090/egg/" + getItem(position)))
+                    .placeholder(android.R.drawable.ic_menu_camera)
+                    .error(android.R.drawable.ic_input_delete)
+                    .resizeDimen(R.dimen.list_detail_image_size, R.dimen.list_detail_image_size)
+                    .centerInside()
+                    .into(vh.mImageView);
+
+            convertView.setTag(vh);
+        }
+        else {
+            vh = (ViewHolder) convertView.getTag();
+        }
+
+        double positionHeight = getPositionRatio(position);
+        int backgroundIndex = position >= mBackgroundColors.size() ?
+                position % mBackgroundColors.size() : position;
+
+        convertView.setBackgroundResource(mBackgroundColors.get(backgroundIndex));
+
+        Log.d(TAG, "getView position:" + position + " h:" + positionHeight);
+
+        vh.txtLineOne.setHeightRatio(positionHeight);
+        vh.txtLineOne.setText(getItem(position) + position);
+
+        return convertView;
+    }
+
+    private double getPositionRatio(final int position) {
+        double ratio = sPositionHeightRatios.get(position, 0.0);
+        // if not yet done generate and stash the columns height
+        // in our real world scenario this will be determined by
+        // some match based on the known height and width of the image
+        // and maybe a helpful way to get the column height!
+        if (ratio == 0) {
+            ratio = getRandomHeightRatio();
+            sPositionHeightRatios.append(position, ratio);
+            Log.d(TAG, "getPositionRatio:" + position + " ratio:" + ratio);
+        }
+        return ratio;
+    }
+
+    private double getRandomHeightRatio() {
+        return (mRandom.nextDouble() / 2.0) + 1.0; // height will be 1.0 - 1.5 the width
+    }
+}
--- Nefete/src/main/java/org/kamol/nefete/ui/fragment/ListAdContainerFragment.java
+++ Nefete/src/main/java/org/kamol/nefete/ui/fragment/ListAdContainerFragment.java
@@ -2,12 +2,32 @@ package org.kamol.nefete.ui.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.etsy.android.grid.StaggeredGridView;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
 import org.kamol.nefete.R;
+import org.kamol.nefete.adapter.SampleAdapter;
+import org.kamol.nefete.http.GoRestClient;
+
+import java.util.ArrayList;
 
 public class ListAdContainerFragment extends Fragment {
+    private static final String TAG = "ListAdContainerFragment";
+    public static final String SAVED_DATA_KEY = "SAVED_DATA";
+    private static final int FETCH_DATA_TASK_DURATION = 2000;
+
+    private StaggeredGridView mGridView;
+    private SampleAdapter mAdapter;
+    private ArrayList<String> mData;
 
     public static ListAdContainerFragment newInstance() {
         return new ListAdContainerFragment();
@@ -15,6 +35,102 @@ public class ListAdContainerFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_container_list_ad, container, false);
+        final View view = inflater.inflate(R.layout.fragment_container_list_ad, container, false);
+        mGridView = (StaggeredGridView) view.findViewById(R.id.grid_view);
+
+        mGridView.setEmptyView(view.findViewById(android.R.id.empty));
+        mAdapter = new SampleAdapter(getActivity(), R.id.txt_line1);
+
+        // TODO: Handle savedInstanceState
+        // do we have saved data?
+//        if (savedInstanceState != null) {
+//            mData = savedInstanceState.getStringArrayList(SAVED_DATA_KEY);
+//            fillAdapter();
+//        }
+
+//        if (mData == null) {
+//            mData = SampleData.generateSampleData();
+//        }
+
+//        for (String data : mData) {
+//            mAdapter.add(data);
+//        }
+
+        mGridView.setAdapter(mAdapter);
+        // TODO: Add listener for OnScroll and OnItemClick to mGridView
+
+        fetchData();
+
+        return view;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public class ListResult {
+        private String status;
+        private Result[] result;
+        private String message;
+
+        public ListResult() {
+            this(null, null);
+        }
+
+        public ListResult(String s, Result[] r) {
+            this.status = s;
+            this.result = r;
+        }
+
+        public Result[] getResult() {
+            return result;
+        }
+
+        public void setResult(Result[] result) {
+            this.result = result;
+        }
+    }
+
+    public class Result {
+        private String title;
+        private String price;
+        private String image;
+    }
+
+    /*
+    POST: http://localhost:8080/list?limit=20
+    {
+	    status: "OK"
+	    result: [
+	        0:{
+	            title: "test"
+	            price: 6468
+	            image: "0001_72a53f664db6f415e9e862c607d9c0ba177c20af_655B4C_100_75"
+	        ...
+        ]
+    }
+    */
+    private void fetchData() {
+        RequestParams p = new RequestParams();
+        p.put("limit", 20);
+        GoRestClient.post(":8080/list", p, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(JSONObject jsonObject) {
+                Log.d(TAG, jsonObject.toString());
+                Gson gson = new GsonBuilder().create();
+                ListResult listResult = gson.fromJson(jsonObject.toString(), ListResult.class);
+                if (listResult.status.equals("OK")) { //if not found it will returns "ERROR"
+                    String imgUrl = "";
+                    for (int i = 0; i < listResult.getResult().length; i++) {
+                        imgUrl = listResult.getResult()[i].image;
+                        Log.d(TAG, "i = " + imgUrl);
+                        if (imgUrl.length() != 0) {
+                            mAdapter.add(imgUrl);
+                        }
+                    }
+                }
+            }
+        });
     }
 }
